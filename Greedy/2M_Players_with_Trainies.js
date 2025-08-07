47cd2a040d2545c5ecff16d156a69d9af3e935cf
soumik171 <soumiksarker8@gmail.com>
2025-08-07 21:38:31 +0600

    Create 2M_Players_with_Trainies.js

---
+++ Greedy/2M_Players_with_Trainies.js
@@ -0,0 +1,23 @@
+var matchPlayersAndTrainers = function (players, trainers) {
+  let pL = players.length;
+  let tL = trainers.length;
+
+  players = new Int32Array(players).sort();
+  trainers = new Int32Array(trainers).sort();
+
+  let pI = 0;
+  let tI = 0;
+  let count = 0;
+
+  while (pI <= pL && tI <= tL) {
+    if (players[pI] <= trainers[tI]) {
+      count++;
+      pI++;
+    }
+    tI++;
+  }
+  return count;
+};
+
+console.log(matchPlayersAndTrainers([4, 7, 9], [8, 2, 5, 8]));
+console.log(matchPlayersAndTrainers([1, 1, 1], [10]));
