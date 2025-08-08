69816d428b0752e19c342da35e7ec17d162ebdbc
soumik171 <soumiksarker8@gmail.com>
2025-08-08 12:46:50 +0600

    Create 4M_Lemonade_Change.js

---
+++ Greedy/4M_Lemonade_Change.js
@@ -0,0 +1,47 @@
+var lemonadeChange = function (bills) {
+  let dollar5 = 0;
+  let dollar10 = 0;
+
+  for (let i = 0; i < bills.length; i++) {
+    if (bills[i] == 5) dollar5++;
+    else if (bills[i] == 10) {
+      dollar10++;
+      if (dollar5 > 0) dollar5--;
+      else return false;
+    } else if (bills[i] == 20) {
+      if (dollar5 > 0 && dollar10 > 0) {
+        dollar5--;
+        dollar10--;
+      } else if (dollar5 >= 3) dollar5 -= 3;
+      else return false;
+    }
+  }
+  return true;
+};
+
+console.log(lemonadeChange([5, 5, 5, 10, 20]));
+console.log(lemonadeChange([5, 5, 10, 10, 20]));
+console.log(
+  lemonadeChange(
+    5,
+    5,
+    10,
+    20,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    10,
+    5,
+    5,
+    20,
+    5,
+    20,
+    5
+  )
+);
