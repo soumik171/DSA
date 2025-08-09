7e9f3312c2075ffc5496d66fa923bdff2b668c96
soumik171 <soumiksarker8@gmail.com>
2025-08-09 15:49:21 +0600

    Create 11M_Check_2_Strings_Close_or_Not.js

---
+++ Strings/11M_Check_2_Strings_Close_or_Not.js
@@ -0,0 +1,39 @@
+var closeStrings = function (word1, word2) {
+  let n1 = word1.length;
+  let n2 = word2.length;
+
+  // as length must be same for two strings to be converted
+  if (n1 != n2) return false;
+
+  let str1 = new Array(26).fill(0);
+  let str2 = new Array(26).fill(0);
+
+  // subtract with 'a' and push that in the newArray & increment that
+  // can also be implemented using for of loop
+  let start = "a".charCodeAt(0);
+  for (let i = 0; i < n1; i++) {
+    str1[word1[i].charCodeAt(0) - start]++; // as in js, char is passed as strings, so it has be converted into char and pass them
+    str2[word2[i].charCodeAt(0) - start]++;
+  }
+
+  //   As elemets are same & frequency can be diff, so the position can have diffrent values bt position must be the same for both the str, so check that
+  for (let i = 0; i < 26; i++) {
+    if ((str1[i] == 0 && str2[i] != 0) || (str1[i] != 0 && str2[i] == 0))
+      return false;
+  }
+  // Sort to check the elements of the arr are same or not
+
+  str1.sort();
+  str2.sort();
+
+  // check the sorted arr indexwise, if it's matched return true or false
+  for (let i = 0; i < 26; i++) {
+    if (str1[i] != str2[i]) return false;
+  }
+  return true;
+};
+
+console.log(closeStrings("abc", "bca"));
+console.log(closeStrings("a", "aa"));
+console.log(closeStrings("cabbba", "abbccc"));
+console.log(closeStrings("abbzzca", "babzzcz"));
